822c8ac311625c6941a69c57d8dcf4425fcf6cc7
Houzhang Liang <houzhang@gmail.com>
2017-12-08 17:10:57 +0000

    added example for uri parameter and query parameter

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -1,9 +1,26 @@
 package controllers
 
 import javax.inject.Inject
-
 import play.api.mvc._
 
 class HelloController  @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+  //  http://localhost:9000/hello
+  def hello = Action {
+    Ok("Hello")
+  }
+
+  //  http://localhost:9000/helloWithName/houzhang
+  def helloWithName(name: String) = Action {
+    Ok(s"Hello ${name}")
+  }
+
+  //  http://localhost:9000/helloWithQueryParams?name=houzhang
+  def helloWithQueryParams = Action { implicit request =>
+    Ok(s"Hello ${request.queryString}")
+  }
+
+  //  def hello = Action(parse.json) { implicit request =>
+  //    Ok("Hello got request [" + request.body + "]")
+  //  }
 }
